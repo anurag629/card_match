d690dff098bc43516f41959d0fd14b1e8bca99a4
Your Name <you@example.com>
2022-01-27 10:34:12 +0530

    all completed

---
+++ src/Pokedex.js
@@ -0,0 +1,29 @@
+import Reeact, { Component } from 'react';
+import Pokecard from './Pokecard';
+import './Pokedex.css';
+
+class Pokedex extends Component {
+    render() {
+        let title;
+        if (this.props.isWinner) {
+            title = <h1 className="Pokedex-winner">Winning Hand</h1>;
+        }
+        else {
+            title = <h1 className="Pokedex-loser">Loosing Hand</h1>;
+        }
+
+        return (
+            <div className='Pokedex'>
+                {title}
+                <h4>Total Experiance: {this.props.exp}</h4>
+                <div className='Pokedex-cards'>
+                    {this.props.pokemon.map((p) => (
+                        <Pokecard id={p.id} name={p.name} type={p.type} exp={p.base_experience} />
+                    ))}
+                </div>
+            </div>
+        );
+    }
+}
+
+export default Pokedex;
