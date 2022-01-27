54dc65b300158d4d790e925a9eb239349cab6dc0
Your Name <you@example.com>
2022-01-27 09:31:04 +0530

    added files

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,9 @@
-import { render, screen } from '@testing-library/react';
+import React from 'react';
+import ReactDOM from 'react-dom';
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+it('renders without crashing', () => {
+  const div = documnet.createElement('div');
+  ReactDom.render(<App />, div);
+  ReactDOM.unmountComponentAtNode(div);
 });
